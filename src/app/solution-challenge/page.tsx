27b007326dca40e6be38d77c8f1ccbcb91f61083
Rouxist <name.of.squaner@gmail.com>
2024-07-24 13:37:11 +0900

    feat: fixed size of solution challenge

--- src/app/solution-challenge/page.tsx
+++ src/app/solution-challenge/page.tsx
@@ -13,10 +13,14 @@ export default function SolutionChallengeList() {
         <Header category={"solution-challenge"} />
       </header>
       <section>
-        <div className="max-w-[1600px] h-[90vh] py-12 grid grid-rows-2 grid-cols-5 gap-y-8 px-20 mx-auto overflow-hidden">
-          {solutionChallengeData.map((data: SolutionChallenge, idx: number) => (
-            <SolutionChallengeCard solutionChallengeData={data} key={idx} />
-          ))}
+        <div className="max-w-[1600px] overflow-scroll mx-auto">
+          <div className="w-[1600px] h-[900px] py-12 grid grid-rows-2 grid-cols-5 gap-y-8 px-20 mx-auto">
+            {solutionChallengeData.map(
+              (data: SolutionChallenge, idx: number) => (
+                <SolutionChallengeCard solutionChallengeData={data} key={idx} />
+              )
+            )}
+          </div>
         </div>
       </section>
     </main>
